a6ad3bea2a8ee36110888602fd7694d49ac5f128
gerardd33 <gerardd@interia.pl>
2020-04-08 06:05:01 +0200

    Added testing; power still floating point exception

--- pix.asm
+++ pix.asm
@@ -66,121 +66,97 @@ section .text
 	shr rbx, 1 ; power /= 2
 	jmp %%powerLoop
 %%endPowerLoop:
-
 %endmacro
 
+; TEST: - floating point exception (core dumped)
+powPix:
+	push rbx
+	power rdi, rsi, rdx
+	pop rbx
+	ret
 
 
 
 
 
 
-; From the tutorial on how to use BBP Formula to calculate the nth pi digit:
-; https://math.stackexchange.com/questions/880904/how-do-you-use-the-bbp-formula-to-calculate-the-nth-digit-of-%CF%80
-; Computes the first sum (16 ^ (n-k) mod 8 k + j ...) in the last line of the tutorial.
-; uint64_t sum1Pix(uint64_t n, uint64_t j)
-; rdi - n
-; rsi - j
-; rax - result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+modPix:
+    ; rdi - wartosc
+    ; rsi - modulo
+
+        ; TODO
+
+        ret
+
 sum1Pix:
-	ret
-
-
-
-
-; Computes the second sum (16 ^ (n-k) mod 8 k + j ...) in the last line of the tutorial (see above).
-; uint64_t sum2Pix(uint64_t n, uint64_t j)
 ; rdi - n
 ; rsi - j
-; rax - result
+        ; TODO
+
+        ret
+
 sum2Pix:
-	ret
-
-	
-
-
-
-; Computes {16^n * S_j} for the blue equation from the tutorial (see above).
-; %1 - j
-; %2 - n
-; %3 (temporarily in rax) - result
-%macro computeSj 3
-	push %2
-	xor rax, rax ; We'll store the result here
-	
-	
-	xor r8, r8 ; k
-; for (k = 0; k <= n; ++k)
-%%computeSjLoop: 
-	cmp r8, %2 ; if (k == n) break
-	je %%endComputeSjLoop
-	
-	
-	jmp %%computeSjLoop
-%%endComputeSjLoop:
-
-	mov %3, rax
-	pop %2
-%endmacro
-
-
-
-; Computes {16^n * pi} from the blue equation from the tutorial (see above).
-; uint64_t pixPi(uint64_t n)
 ; rdi - n
-; rax - result
+; rsi - j
+        ; TODO
+
+        ret
+
 pixPi:
-	push r12
-	push r13
-	push r14
-	push r15
-	
-	computeSj 1, rdi, r12 ; S1
-	computeSj 4, rdi, r13 ; S4
-	computeSj 5, rdi, r14 ; S5
-	computeSj 6, rdi, r15 ; S6
-	
-	; ??? Co tutaj z wychodzeniem ponizej zera i powyzej overflowa?
-	xor rax, rax
-	add rax, r12 ; += S1
-	add rax, r12 ; += S1
-	sub rax, r13 ; -= S4
-	sub rax, r13 ; -= S4
-	add rax, r12 ; += S1
-	add rax, r12 ; += S1
-	sub rax, r14 ; -= S5
-	sub rax, r15 ; -= S6
-	
-	pop r12
-	pop r13
-	pop r14
-	pop r15
-	ret
+; rdi - n
 
+        ; TODO
 
+        ret
 
-; TODO: usun 
-; Checks correctness of using mutexes. You should increment the given index in the array,
-; instead of writing there the target value.
-; void pwPix(uint32_t *ppi, uint64_t *pidx, uint64_t max)
-; rdi - pointer to the ppi array 
-; rsi - pointer to the pidx index
-; rdx - the max value
+; TODO do wyjebonexa
 align 8
 pwPix:
-	ret
+; rdi - wskaznik na tablice
+; rsi - wskaznik na indeks
+; rdx - wartosc max
 
+        ; TODO
+
+        ret
 
 
 align 8 ; TODO: usun jak bedzie dzialac bez
 pix:
 	ret
-
-
-
-
-
-
 
 
 
